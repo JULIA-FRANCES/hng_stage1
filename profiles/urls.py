cf3a05969e5533d86c54d39e7f5afa71a831ff38
JULIA-FRANCES <juliafranceskosisochukwu2@gmail.com>
2026-04-17 14:21:53 +0100

    fix api url prefix

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from profiles import views
 
 urlpatterns = [
-    path('profiles', views.profiles_router, name='profiles'),
-    path('profiles/<uuid:pk>', views.profile_detail, name='profile-detail'),
+    path('api/profiles', views.profiles_router, name='profiles'),
+    path('api/profiles/<str:pk>', views.profile_detail, name='profile-detail'),
 ]
