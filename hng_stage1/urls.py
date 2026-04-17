78710e887ff2c8dddcaf4aea9adaae9834b06780
JULIA-FRANCES <juliafranceskosisochukwu2@gmail.com>
2026-04-17 14:28:00 +0100

    add custom json 404 handler

--- hng_stage1/urls.py
+++ hng_stage1/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path, include
+from django.http import JsonResponse
+
+def handler404(request, exception):
+    return JsonResponse(
+        {"status": "error", "message": "Profile not found."},
+        status=404
+    )
 
 urlpatterns = [
     path('', include('profiles.urls')),
